8e88d7a6b93d733fae8929a144b4fef4c87442bb
viperluan <viperluan@gmail.com>
2022-03-10 03:42:32 -0300

    Added state, imported MenuDrawer and updated styles.

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,10 +1,25 @@
 import Link from 'next/link';
 
+import { MouseEvent, useState } from 'react';
+
 import ActiveLink from 'src/components/Utils/ActiveLink';
+
+import MenuDrawer from '../MenuDrawer/MenuDrawer';
 
 import styles from './Header.module.scss';
 
 const Header = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
+
+  const handleClickMenu = (mouseEvent: MouseEvent<HTMLButtonElement>) => {
+    mouseEvent.preventDefault();
+    setIsDrawerOpen(true);
+  };
+
+  const handleRequestClose = () => {
+    setIsDrawerOpen(false);
+  };
+
   return (
     <>
       <header className={styles.container}>
@@ -12,6 +27,7 @@ const Header = () => {
           <span>
             <Link href="/">LCS</Link>
           </span>
+
           <nav>
             <ul>
               <li>
@@ -36,8 +52,14 @@ const Header = () => {
               </li>
             </ul>
           </nav>
+
+          <button type="button" onClick={handleClickMenu}>
+            <span className={`${isDrawerOpen && styles.active}`} />
+          </button>
         </div>
       </header>
+
+      <MenuDrawer isActive={isDrawerOpen} handleRequestClose={handleRequestClose} />
     </>
   );
 };
