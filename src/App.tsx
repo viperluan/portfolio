7425d9e34a74280ddc8b3280b78b7c5c01c2fa1d
viperluan <viperluan@gmail.com>
2024-02-12 07:47:54 -0300

    added fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import "./App.sass";
+import "./App.scss";
+import "@fontsource/jetbrains-mono";
+import "@fontsource/jetbrains-mono/100.css";
+import "@fontsource/jetbrains-mono/700.css";
 import { Container } from "./components/Container";
 import { Header } from "./components/Header";
+import { Section } from "./components/Section";
+import { Action } from "./components/Section/Action";
 
 function App() {
   return (
     <>
       <Container>
         <Header />
+        <Section>
+          <Action />
+        </Section>
       </Container>
     </>
   );
