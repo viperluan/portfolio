f2de02dfdf23c442d7ca6122810eee7c85aba561
viperluan <viperluan@gmail.com>
2024-02-12 06:40:19 -0300

    create logo component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,13 @@
+import "./styles.scss";
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <a href="/">
+        <img src="src/assets/logo.svg" alt="Logo LCS" />
+      </a>
+    </div>
+  );
+};
+
+export { Logo };
