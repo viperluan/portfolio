dff55c4cee8ee1e1778c4e9a5de4943c7d510007
viperluan <viperluan@gmail.com>
2024-02-14 12:07:24 -0300

    created mobile menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,31 @@
+import { MouseEvent, useState } from 'react';
 import { Logo } from '../Logo';
 import { Navigator } from '../Navigator';
+import { MenuMobile } from '../MenuMobile';
 import './styles.scss';
 
 const Header = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const handleClickMenu = (mouseEvent: MouseEvent<HTMLButtonElement>) => {
+    mouseEvent.preventDefault();
+    setMenuIsOpen(!menuIsOpen);
+  };
+
+  const handleRequestClose = () => {
+    setMenuIsOpen(false);
+  };
+
   return (
     <div className="header">
       <Logo />
       <Navigator />
+
+      <button className="menu-button" onClick={handleClickMenu}>
+        <span className={`${menuIsOpen ? 'active' : 'inactive'}`}></span>
+      </button>
+
+      <MenuMobile isOpen={menuIsOpen} handleRequestClose={handleRequestClose} />
     </div>
   );
 };
---
+++ src/components/MenuMobile/index.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react';
+import './styles.scss';
+
+interface MenuMobileProps {
+  isOpen: boolean;
+  handleRequestClose: () => void;
+}
+
+const MenuMobile = ({ isOpen, handleRequestClose }: MenuMobileProps) => {
+  const [html, setHtml] = useState<HTMLElement | null>(null);
+  const [menu, setMenu] = useState<HTMLElement | null>(null);
+
+  const handleClickOutside = (htmlEvent: MouseEvent) => {
+    if (htmlEvent) {
+      if (menu?.contains(htmlEvent.target as Node)) {
+        handleRequestClose();
+        html?.removeEventListener('click', handleClickOutside);
+      }
+    }
+  };
+
+  const handleClick = () => {
+    html?.removeEventListener('click', handleClickOutside);
+    handleRequestClose();
+  };
+
+  useEffect(() => {
+    if (!html || !menu) {
+      const htmlElement = document.documentElement;
+      const menuElement = document.getElementById('menu');
+
+      if (htmlElement && menuElement) {
+        setHtml(htmlElement);
+        setMenu(menuElement);
+      }
+    }
+
+    if (isOpen && html) {
+      html.addEventListener('click', handleClickOutside);
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isOpen]);
+
+  return (
+    <div id="menu" className={`menu-mobile-container ${isOpen ? 'active' : 'inactive'}`}>
+      <nav className="nav-container">
+        <ul>
+          <li onClick={handleClick}>
+            <a href="#about">about</a>
+          </li>
+          <li onClick={handleClick}>
+            <a href="#works">works</a>
+          </li>
+          <li onClick={handleClick}>
+            <a href="#contact">contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export { MenuMobile };
