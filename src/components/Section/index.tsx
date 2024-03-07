b1107c44cf93e45a7591644513464576efa0218f
viperluan <viperluan@gmail.com>
2024-03-07 17:21:58 -0300

    added intersection effects

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -1,8 +1,18 @@
 import './styles.scss';
+
+import { useInView } from 'react-intersection-observer';
 import { PropsWithChildren } from 'react';
 
 const Section = ({ children }: PropsWithChildren) => {
-  return <div className="section">{children}</div>;
+  const { ref, inView } = useInView({ threshold: 0.5, triggerOnce: true });
+
+  const animationClassName = inView ? 'inview' : '';
+
+  return (
+    <div ref={ref} className={`section ${animationClassName}`}>
+      {children}
+    </div>
+  );
 };
 
 export { Section };
--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,8 +1,17 @@
 import { PropsWithChildren } from 'react';
+import { useInView } from 'react-intersection-observer';
 import './styles.scss';
 
 const Title = ({ children }: PropsWithChildren) => {
-  return <h1 className="title">{children}</h1>;
+  const { ref, inView } = useInView({ threshold: 1, triggerOnce: true });
+
+  const animationClassName = inView ? 'inview' : '';
+
+  return (
+    <h1 ref={ref} className={`title ${animationClassName}`}>
+      {children}
+    </h1>
+  );
 };
 
 export { Title };
