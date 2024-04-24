dc064103cd630f5a6135287199179e0cd20f14fe
viperluan <viperluan@gmail.com>
2024-04-24 19:08:11 -0300

    adiciona main no app e fonte poppins

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.scss';
 import { MenuMobile } from './components/layout/menuMobile';
+import { Main } from './components/layout/section/main';
 
 function App() {
   return (
     <div className="app-container">
       <MenuMobile />
+
+      <Main />
     </div>
   );
 }
