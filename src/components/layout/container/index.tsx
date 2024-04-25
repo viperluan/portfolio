1cb9492ed1c843f1e56d8124fe0b3e9592fa8563
viperluan <viperluan@gmail.com>
2024-04-24 22:31:57 -0300

    deleta componente container

--- src/components/layout/container/index.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-import './styles.scss';
-
-const Container = ({ children }: PropsWithChildren) => {
-  return <div className="container">{children}</div>;
-};
-
-export { Container };
