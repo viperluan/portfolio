563df6e47b33e03a8b06233e3130d600668e8c78
viperluan <viperluan@gmail.com>
2024-05-14 01:03:26 -0300

    corrige fontsize da section main

--- src/components/layout/section/main/index.tsx
+++ src/components/layout/section/main/index.tsx
@@ -4,31 +4,33 @@ const Main = () => {
   return (
     <>
       <main>
-        <p className="main-tag">Desenvolvedor Web & Mobile</p>
+        <div className="main-content-container">
+          <p className="main-tag">Desenvolvedor Web & Mobile</p>
 
-        <h1 className="main-title">
-          Bem vindo! Meu nome é <span>Luan</span>.
-        </h1>
+          <h1 className="main-title">
+            Bem vindo! Meu nome é <span>Luan</span>.
+          </h1>
 
-        <p className="main-text">
-          Crio soluções consistentes, inteligentes e seguras para impulsionar o futuro do seu
-          empreendimento.
-        </p>
+          <p className="main-text">
+            Crio soluções consistentes, inteligentes e seguras para impulsionar o futuro do seu
+            empreendimento.
+          </p>
 
-        <div className="main-buttons-container">
-          <a className="main-button-curriculum" href="">
-            <button>Baixar currículo</button>
-          </a>
+          <div className="main-buttons-container">
+            <a className="main-button-curriculum" href="">
+              <button>Baixar currículo</button>
+            </a>
 
-          <a className="main-button-contact" href="">
-            <button>Contato</button>
-          </a>
-        </div>
+            <a className="main-button-contact" href="">
+              <button>Contato</button>
+            </a>
+          </div>
 
-        <div className="scroll-down-container">
-          <span className="first-scroll-down">{'>'}</span>
-          <span className="second-scroll-down">{'>'}</span>
-          <span className="third-scroll-down">{'>'}</span>
+          <div className="scroll-down-container">
+            <span className="first-scroll-down">{'>'}</span>
+            <span className="second-scroll-down">{'>'}</span>
+            <span className="third-scroll-down">{'>'}</span>
+          </div>
         </div>
       </main>
     </>
