d094d0bdbc5df750c07f07c8bd1113eed329683a
viperluan <viperluan@gmail.com>
2024-05-14 22:56:52 -0300

    ajusta responsividade footer

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,7 +1,11 @@
 import './styles.scss';
 
 const Footer = () => {
-  return <footer className="footer-container">© LCS Soluções em Tecnologia LTDA - 2024</footer>;
+  return (
+    <footer className="footer-container">
+      <p className="footer-text-container">© LCS Soluções em Tecnologia LTDA - 2024</p>
+    </footer>
+  );
 };
 
 export { Footer };
